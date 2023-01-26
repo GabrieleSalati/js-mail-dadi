a9e2082198f08ba4e3d0c7f78348d0b374863318
GabrieleSalati <gabriele.salati5@gmail.com>
2023-01-26 16:22:06 +0100

    email collection function added

---
+++ mail/js/main.js
@@ -0,0 +1,20 @@
+//VARIABLE
+
+let userEmail = document.getElementById("mail");
+
+//DATA COLLECTOR
+
+send.addEventListener(
+    "click",
+    function() {
+        
+    //USER EMAIL REQUEST
+    
+    let email = userEmail.value;
+
+    console.log(email);
+    
+}
+
+   
+)
